4467dedeb1c8781c1046915939f6ea767cbd0fbd
Akshit Jain <146845857+akshitjain1@users.noreply.github.com>
2025-07-24 10:23:19 +0530

    Time: 0 ms (100%), Space: 14.9 MB (20.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,14 +3,15 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> map;
 
-        for (int i = 0; i < nums.size(); i++) {
-            int b = target - nums[i];
+        for (int i =0; i<nums.size(); i++) {
+            int b =target-nums[i];
+
             if (map.count(b)) {
-                return {map[b], i};
+                return{map[b],i};
             }
-            map[nums[i]] = i;
+            map[nums[i]] =i;
         }
 
-        return {-1, -1}; 
+        return {-1,-1}; 
     }
 };
