6caefa2fa90807455f95ffb72a62bcda8d484c30
Akshit Jain <146845857+akshitjain1@users.noreply.github.com>
2025-10-07 07:34:27 +0530

    Time: 13 ms (31.4%), Space: 7.9 MB (36.03%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,29 @@
-class Solution {
-public:
-    int fib(int n) {
-        int arr[n+1];
-        if(n<=1)return n;
-        arr[0]=0;
-        arr[1]=1;
-        for(int i=2;i<=n;i++){
-            arr[i]= arr[i-1]+arr[i-2];
-        }
-        return arr[n];
+// Using linear Method
+// class Solution {
+// public:
+//     int fib(int n) {
+//         int arr[n+1];
+
+//         if(n<=1)return n;
+        
+//         arr[0]=0;
+//         arr[1]=1;
+//         for(int i=2;i<=n;i++){
+//             arr[i]= arr[i-1]+arr[i-2];
+//         }
+//         return arr[n];
+//     }
+// };
+
+
+// Multiple Recursion Calls
+class Solution{
+    public:
+    int f(int n){
+        if(n<=1) return n;
+        return f(n-1)+f(n-2);
+    }
+    int fib(int n){
+        return f(n);
     }
 };
