20c933dd2e16f41101b2b9308d0fa39703c96705
Akshit Jain <146845857+akshitjain1@users.noreply.github.com>
2026-03-17 18:39:35 +0530

    Time: 0 ms (100%), Space: 10.3 MB (23.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    bool f(string &s , int i){
-        if(i>=s.size()/2)return true;
-        if(s[i] != s[s.size()-i-1])return false;
-        return f(s, i+1);
+    bool check(const string& s, int left, int right) {
+        if (left >= right) return true; 
+        
+        if (s[left] != s[right]) return false; 
+        
+        return check(s, left + 1, right - 1); 
     }
+
     bool isPalindrome(string s) {
         string s2 = "";
-        for(char c : s){
-            if(isalnum(c)){
-                s2.push_back(tolower(c));
-            }
+        for(char c : s) {
+            if(isalnum(c)) s2.push_back(tolower(c));
         }
-        return f(s2, 0);
+        if (s2.empty()) return true;
+        return check(s2, 0, s2.size() - 1);
     }
 };
