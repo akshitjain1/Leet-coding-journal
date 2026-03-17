6eaba6b03774f6c0d4a4ed2aaeada3dc588adc43
Akshit Jain <146845857+akshitjain1@users.noreply.github.com>
2026-03-17 18:43:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,29 +1,29 @@
-// // Using linear Method
-// class Solution {
-// public:
-//     int fib(int n) {
-//         int arr[n+1];
+// Using linear Method
+class Solution {
+public:
+    int fib(int n) {
+        int arr[n+1];
 
-//         if(n<=1)return n;
+        if(n<=1)return n;
         
-//         arr[0]=0;
-//         arr[1]=1;
-//         for(int i=2;i<=n;i++){
-//             arr[i]= arr[i-1]+arr[i-2];
-//         }
-//         return arr[n];
-//     }
-// };
-
-
-// Multiple Recursion Calls : Time Complexity would be higher due to recursion calls and space complxity is also higher : cuz stack remain intact untill all the recursion calls completes.
-class Solution{
-    public:
-    int f(int n){
-        if(n<=1) return n;
-        return f(n-1)+f(n-2);
-    }
-    int fib(int n){
-        return f(n);
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;i++){
+            arr[i]= arr[i-1]+arr[i-2];
+        }
+        return arr[n];
     }
 };
+
+
+// // Multiple Recursion Calls : Time Complexity would be higher due to recursion calls and space complxity is also higher : cuz stack remain intact untill all the recursion calls completes.
+// class Solution{
+//     public:
+//     int f(int n){
+//         if(n<=1) return n;
+//         return f(n-1)+f(n-2);
+//     }
+//     int fib(int n){
+//         return f(n);
+//     }
+// };
