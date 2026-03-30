29c0207fa6d95b34839902174532a69b601372fa
Akshit Jain <146845857+akshitjain1@users.noreply.github.com>
2026-03-30 14:09:18 +0530

    Time: 4 ms (64.99%), Space: 53.2 MB (56.94%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-         vector<int> miss;
-        int n = nums.size();
-        int i =0;
-        while (i < n) {
-            int correctIndex = nums[i]-1;
-            if (nums[i] != nums[correctIndex]) {
-                std::swap(nums[i], nums[correctIndex]);
-            } else {
-                i++;
+        vector<int> result;
+
+        // Step 1: Iterate through the array and mark visited numbers
+        for (int i = 0; i < nums.size(); i++) {
+            // Get the value (as an index). Use abs() because it might already be negative.
+            int index = abs(nums[i]) - 1; 
+            
+            // Mark the value at 'index' as negative to show we've "seen" the number (index + 1)
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
             }
         }
 
-        for (int j = 0; j < n; j++) {
-            if (nums[j] != j+1) {
-            miss.push_back(j+1);
-                
+        // Step 2: Iterate again to find which indices are still positive
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                // If nums[i] is positive, it means (i + 1) never appeared in the array
+                result.push_back(i + 1);
             }
         }
-        return miss;
+
+        return result;
     }
 };
