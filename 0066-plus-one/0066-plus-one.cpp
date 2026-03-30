d1142ca03bae62035306e897eee56b85fec3fe02
Akshit Jain <146845857+akshitjain1@users.noreply.github.com>
2026-03-30 14:16:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,17 +1,20 @@
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-         for (int i = digits.size() - 1; i >= 0; i--) {
-            if (digits[i] + 1 != 10) {
-                digits[i] += 1;
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
                 return digits;
             }
             digits[i] = 0;
-            if (i == 0) {
-                digits.insert(digits.begin(), 1);
-                return digits;
-            }
         }
-        return digits;        
+        
+        digits.insert(digits.begin(), 1);
+        return digits;
     }
 };
