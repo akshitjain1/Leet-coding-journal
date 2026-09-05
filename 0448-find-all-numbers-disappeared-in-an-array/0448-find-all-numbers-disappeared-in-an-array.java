476665a13ea5905e6e67b976c6923da9d0539aae
Akshit Jain <146845857+akshitjain1@users.noreply.github.com>
2026-09-05 15:50:31 +0530

    Time: 6 ms (84.74%), Space: 67.1 MB (73.55%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,17 +1,38 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Boolean> map = new HashMap<>();
-        ArrayList<Integer> result = new ArrayList<>();
-        for(int num : nums){
-            map.put(num, true);
+        List<Integer> result = new ArrayList<>();
+
+        for (int number : nums) {
+            int value = Math.abs(number);
+            int index = value - 1;
+            nums[index] = -Math.abs(nums[index]); // Position becomes the presence marker.
         }
-        for(int i =1;i<=n;i++){
-            if(map.getOrDefault(i,false)){
-                continue;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
             }
-            result.add(i);
         }
+
         return result;
     }
 }
+
+// // Can be a Solution but Here the Space complexity is O(n);
+// class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+//         int n = nums.length;
+//         Map<Integer, Boolean> map = new HashMap<>();
+//         ArrayList<Integer> result = new ArrayList<>();
+//         for(int num : nums){
+//             map.put(num, true);
+//         }
+//         for(int i =1;i<=n;i++){
+//             if(map.getOrDefault(i,false)){
+//                 continue;
+//             }
+//             result.add(i);
+//         }
+//         return result;
+//     }
+// }
