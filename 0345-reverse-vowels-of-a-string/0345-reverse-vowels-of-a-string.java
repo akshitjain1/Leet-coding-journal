b1ee889540ee6f1f5a878a42327391cdcc461b38
Akshit Jain <146845857+akshitjain1@users.noreply.github.com>
2026-09-10 20:14:41 +0530

    Time: 3 ms (64.08%), Space: 46.6 MB (64.14%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,41 +1,57 @@
 class Solution {
-    public void reverse(ArrayList<Character> list){
-        int first = 0;
-        int last = list.size() - 1;
-
-        while(first < last){
-            char temp = list.get(first);
-            list.set(first, list.get(last));
-            list.set(last, temp);
-            first++;
-            last--;
-        }
+    public Boolean isVowel(char c){
+        return "aeiouAEIOU".indexOf(c) != -1;
     }
 
     public String reverseVowels(String s) {
-        String vow = "aeiouAEIOU";
-        ArrayList<Character> list = new ArrayList<>();
-
-        for(char c : s.toCharArray()){
-            if(vow.indexOf(c) != -1){
-                list.add(c);
+        char[] c = s.toCharArray();
+        int left = 0;
+        int right = c.length -1;
+        while(left<right){
+            while(left<right && !isVowel(c[left])){
+                left++;
             }
-        }
-
-        // reverse(list);
-
-        int n = list.size() - 1;
-        StringBuilder sb = new StringBuilder();
-
-        for(char c : s.toCharArray()){
-            if(vow.indexOf(c) != -1){
-                sb.append(list.get(n));
-                n--;
-            }else{
-                sb.append(c);
+            while(left<right && !isVowel(c[right])){
+                right--;
             }
+            if(left < right){
+                char temp = c[left];
+                c[left] = c[right];
+                c[right] =temp;
+            }
+            left++;
+            right--;
         }
-
-        return sb.toString();
+        return new String(c);
     }
 }
+
+// //Valid But o(n) space complexity
+// class Solution {
+
+//     public String reverseVowels(String s) {
+//         String vow = "aeiouAEIOU";
+//         ArrayList<Character> list = new ArrayList<>();
+
+//         for(char c : s.toCharArray()){
+//             if(vow.indexOf(c) != -1){
+//                 list.add(c);
+//             }
+//         }
+
+
+//         int n = list.size() - 1;
+//         StringBuilder sb = new StringBuilder();
+
+//         for(char c : s.toCharArray()){
+//             if(vow.indexOf(c) != -1){
+//                 sb.append(list.get(n));
+//                 n--;
+//             }else{
+//                 sb.append(c);
+//             }
+//         }
+
+//         return sb.toString();
+//     }
+// }
