8f5c049d512464d14cf6ef9f22c279f3b34637a0
Akshit Jain <146845857+akshitjain1@users.noreply.github.com>
2026-09-10 20:38:36 +0530

    Time: 1 ms (25.96%), Space: 43.2 MB (10.98%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,5 +1,24 @@
+// class Solution {
+//     public String toLowerCase(String s) {
+//         return s.toLowerCase();
+//     }
+// }
+
 class Solution {
+
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : s.toCharArray()){
+
+            if(c >= 'A' && c <= 'Z'){
+                c = (char)(c + 32);
+            }
+
+            sb.append(c);
+        }
+
+        return sb.toString();
     }
 }
