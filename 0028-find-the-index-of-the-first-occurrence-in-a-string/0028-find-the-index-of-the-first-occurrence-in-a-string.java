cd4709c72a8f09203eb902c3a7cc6299701c984c
Akshit Jain <146845857+akshitjain1@users.noreply.github.com>
2026-09-11 19:16:15 +0530

    Time: 1 ms (49.6%), Space: 43.3 MB (18.49%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,7 +1,28 @@
 class Solution {
+
+    public boolean contains(String haystack, String needle) {
+        return indexOf(haystack, needle) != -1;
+    }
+    public int indexOf(String haystack, String needle) {
+        if(needle.length() == 0){
+            return 0;
+        }
+        for(int i = 0; i <= haystack.length() - needle.length(); i++){
+            int j = 0;
+            while(j < needle.length() &&
+                  haystack.charAt(i + j) == needle.charAt(j)){
+                j++;
+            }
+            if(j == needle.length()){
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public int strStr(String haystack, String needle) {
-        if(haystack.contains(needle)){
-            return haystack.indexOf(needle);
+        if(contains(haystack, needle)){
+            return indexOf(haystack, needle);
         }
         return -1;
     }
