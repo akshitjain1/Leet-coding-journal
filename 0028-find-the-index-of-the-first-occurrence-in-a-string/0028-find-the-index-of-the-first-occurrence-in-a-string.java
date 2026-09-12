374755201f830459ca37e4997de1a718d71ab66e
Akshit Jain <146845857+akshitjain1@users.noreply.github.com>
2026-09-12 15:19:28 +0530

    Time: 1 ms (49.58%), Space: 43.2 MB (18.57%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,28 +1,23 @@
 class Solution {
-
-    public boolean contains(String haystack, String needle) {
-        return indexOf(haystack, needle) != -1;
+    public boolean contains(String haystack, String needle){
+        return indexOf(haystack,needle) != -1;
     }
-    public int indexOf(String haystack, String needle) {
-        if(needle.length() == 0){
-            return 0;
-        }
-        for(int i = 0; i <= haystack.length() - needle.length(); i++){
+    public int indexOf(String s1, String s2){
+        if(s2.length()==0)return 0;
+        for(int i =0;i<=s1.length()-s2.length();i++){
             int j = 0;
-            while(j < needle.length() &&
-                  haystack.charAt(i + j) == needle.charAt(j)){
+            while(j<s2.length() && s1.charAt(i+j)==s2.charAt(j) ){
                 j++;
             }
-            if(j == needle.length()){
+            if(j==s2.length()){
                 return i;
             }
         }
         return -1;
     }
-
     public int strStr(String haystack, String needle) {
-        if(contains(haystack, needle)){
-            return indexOf(haystack, needle);
+        if(contains(haystack,needle)){
+            return indexOf(haystack,needle);
         }
         return -1;
     }
